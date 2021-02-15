94ad5a8ddfddb9fd95ee20fb1fe6654307df1567
Jiří Vrba <jiri@vrba.dev>
2021-02-15 16:13:49 +0100

    Changed coin weight to 1

--- src/main/java/dev/vrba/vse/adventure/game/entity/items/Coins.java
+++ src/main/java/dev/vrba/vse/adventure/game/entity/items/Coins.java
@@ -4,7 +4,7 @@ public class Coins implements PickableItem {
 
     private final int amount;
 
-    private static final int COIN_WEIGHT = 2;
+    private static final int COIN_WEIGHT = 1;
 
     public Coins(int amount) {
         this.amount = amount;
