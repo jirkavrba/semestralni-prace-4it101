1ac7bce31b6c978bd0f94499749ce25e2aeb7ae9
Jiří Vrba <jiri@vrba.dev>
2021-02-15 18:59:23 +0100

    Added tests for HelpCommand

---
+++ src/test/java/dev/vrba/vse/adventure/game/commands/HelpCommandTest.java
@@ -0,0 +1,23 @@
+package dev.vrba.vse.adventure.game.commands;
+
+import dev.vrba.vse.adventure.game.DungeonGame;
+import dev.vrba.vse.adventure.game.GameTest;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HelpCommandTest extends GameTest {
+    @Test
+    public void testCommandHasExpectedName() {
+        Command command = new HelpCommand();
+        assertEquals("napoveda", command.getName());
+    }
+
+    @Test
+    public void testCanBeInvokedWithoutArguments() {
+        DungeonGame game = createGameWithMockedCommandPrompt();
+        Command command = new HelpCommand();
+
+        assertDoesNotThrow(() -> command.execute(game));
+    }
+}
